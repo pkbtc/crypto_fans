098f33cc92fc347fb72ed5ea41c81f396bae82ad
pkbtc <parasarkashyap06@gmail.com>
2024-08-01 23:10:36 +0530

    fix

--- src/app/creators/[address]/page.js
+++ src/app/creators/[address]/page.js
@@ -2,6 +2,7 @@
 "use client"
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import abi from "./abi.json";
 const getCreator = async (address) => {
     let res = await fetch(
         `/api/creators/${address}`,
@@ -22,6 +23,8 @@ export default function Creator({ params }) {
     const [creator, setCreator] = useState(null);
     const [signature,setSignature]=useState(null);
     const [signer,setSigner]=useState(null);
+    const [contract,setContract]=useState(null);
+    const [isMember,setIsMember]=useState(undefined);
     const message='welcome to the site';
     useEffect(() => {
         const init = async () => {
@@ -37,8 +40,12 @@ export default function Creator({ params }) {
             setSigner(signer);
          const sign=await   signer.signMessage(message);
          setSignature(sign);    
-         console.log(sign);
-        }
+         const contractAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3";
+         const contract =new ethers.Contract(contractAddress,abi,signer);
+         setContract(contract);
+         const res=await contract
+         
+            }
         else{
             alert("Please install Metamask");
         }

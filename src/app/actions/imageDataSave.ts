ddca968eda3d6c7d90df784fb2179be69b5cf81d
pkbtc <parasarkashyap06@gmail.com>
2024-08-10 23:02:19 +0530

    ini

---
+++ src/app/actions/imageDataSave.ts
@@ -0,0 +1,8 @@
+"use server"
+import prisma from "@/lib/db";
+
+
+
+export const createImageData=async(imageData)=>{
+    console.log(imageData);
+}
--- src/components/ui/UplaodImage.tsx
+++ src/components/ui/UplaodImage.tsx
@@ -1,55 +1,85 @@
 "use client"
-import { UploadButton ,UploadDropzone} from '@/utils/uploadthing';
+import { UploadButton, UploadDropzone } from '@/utils/uploadthing';
 import Image from 'next/image';
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { imageRemove } from '@/app/actions/imageRemove';
-
-const UplaodImage = () => {
+import { createImageData } from '@/app/actions/imageDataSave';
+const UploadImage = () => {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
   const [imageKey, setImageKey] = useState<string | null>(null);
-  const handleImage = async() => {
-     const res= await imageRemove(imageKey);
-     if(res.sucess){
-      alert("image removed");
-      setImageUrl('');
-      setImageKey('');
-     }
-     
-  }
+  const [imageTitle, setImageTitle] = useState<string | null>(null);
 
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <UploadDropzone
-            endpoint="imageUploader"
-            onClientUploadComplete={(res) => {
-              // Do something with the response
-              console.log("Files: ", res);
-              setImageUrl(res[0].url);
-              setImageKey(res[0].key);
-              alert("Upload Completed");
-            }}
-            onUploadError={(error: Error) => {
-              // Do something with the error.
-              alert(`ERROR! ${error.message}`);
-            }}
-          />
+  const handleImage = async () => {
+    const res = await imageRemove(imageKey);
+    if (res.sucess) {
+      alert("Image removed");
+      setImageUrl(null);
+      setImageKey(null);
+    }
+    
+  };
+
+  const handleFormSubmit = async(e: React.FormEvent) => {
+    e.preventDefault();
+    const imageData = {
+      imageUrl,
+      imageTitle,
+      imageKey
+    };
+    await createImageData(imageData);
+    console.log(imageData);
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            <label>Image title</label>
+            <input
+              type='text'
+              value={imageTitle || ''}
+              onChange={(e) => setImageTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            <UploadDropzone
+              endpoint="imageUploader"
+              onClientUploadComplete={(res) => {
+                console.log("Files: ", res);
+                setImageUrl(res[0].url);
+                setImageKey(res[0].key);
+                alert("Upload Completed");
+              }}
+              onUploadError={(error: Error) => {
+                alert(`ERROR! ${error.message}`);
+              }}
+            />
+          </div>
           <div>
             <h1 className='text-3xl font-semibold text-gray-800'>Image Preview</h1>
-            {
-              imageUrl && (
-                <div>
-                  <Image src={imageUrl} alt='image uplaod' height={200} width={300} className='object-cover h-auto w-full'/>
-
-                </div>
-              )
-            }
+            {imageUrl && (
+              <div>
+                <Image
+                  src={imageUrl}
+                  alt='image upload'
+                  height={200}
+                  width={300}
+                  className='object-cover h-auto w-full'
+                />
+              </div>
+            )}
             <div>
-              <button onClick={handleImage}>remove image</button>
+              <button type="button" onClick={handleImage}>Remove Image</button>
             </div>
           </div>
-        </main>
-      );
-}
+          <div>
+            <button type='submit'>Save Image</button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
 
-export default UplaodImage
+export default UploadImage;

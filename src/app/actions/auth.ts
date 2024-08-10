710e9b24449a38c7610a23f9c82886e609842437
pkbtc <parasarkashyap06@gmail.com>
2024-08-11 01:34:18 +0530

    added signup

---
+++ src/app/actions/auth.ts
@@ -0,0 +1,8 @@
+"use server"
+import prisma from "@/lib/db"
+export async function createUser(userData){
+    const {email,password}=userData;
+    await prisma.sign.create({
+        data:userData
+    });
+}
--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,6 +1,20 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
+import bcrypt from "bcryptjs";
+import { createUser } from "@/app/actions/auth";
 
-export default function SignUp() {
+export default function SignUp() {  
+  const [email,setEmail]=useState("");
+  const [Password,setPassoword]=useState("");
+  const handleSignUp=async()=>{
+    const password=bcrypt.hashSync(Password,10);
+    const userData={
+      email,
+      password
+    }
+    await createUser(userData);
+  }
   return (
     <div>
       <form className="flex flex-col p-4 gap-2">
@@ -11,15 +25,17 @@ export default function SignUp() {
           id="email"
           name="email"
           placeholder="Email"
+          onChange={(e)=>setEmail(e.target.value)}
         />
         <input
           className="border border-gray-300 rounded-lg  text-lg  p-2"
-          type="text"
+          type="password"
           id="password"
           name="password"
           placeholder="Password"
+          onChange={(e)=>setPassoword(e.target.value)}
         />
-        <button className="rounded-2xl text-center bg-gray-200 p-1 text-sm">
+        <button onClick={handleSignUp} className="rounded-2xl text-center bg-gray-200 p-1 text-sm">
           SIGN Up
         </button>
       </form>

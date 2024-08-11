52a3b25507095a10a1562ac3beeb0613955fb1dc
pkbtc <parasarkashyap06@gmail.com>
2024-08-11 19:28:13 +0530

    added dynamic user creation

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -2,12 +2,15 @@ import LatestFeeds from "@/components/LatestFeeds";
 import HomeBox from "@/components/HomeBox";
 import ComposeNewPost from "@/components/ComposeNewPost";
 import Auth from "@/components/Auht"
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { createUser } from "./actions/createUser";
 export default async function Home() {
-  const { userId } = auth();
-  console.log(userId);
-  const user = await currentUser()
-  console.log(user);
+  // const { userId } = auth();
+  // console.log("userId:",userId);
+  // const user = await currentUser()
+  // console.log("User Data:",user);
+  // console.log("url:",user.imageUrl);
+  // console.log(user.emailAddresses[0].emailAddress);
+  await createUser();
   return (
     <>
       <HomeBox />
